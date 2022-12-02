c7ac761f95d8bdcac0082940c81d7e73a91b3e63
Ana Hidalgo <119543207+luzcaraballo@users.noreply.github.com>
2022-12-02 19:56:17 -0300

    Update streamlit_app.py
    added comments

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,9 +25,9 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
-# write your own comment -what does the next line do? 
+# normalizes the json response 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
+# formats the result as table
 streamlit.dataframe(fruityvice_normalized)
